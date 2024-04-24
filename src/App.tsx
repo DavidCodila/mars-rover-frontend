87de7045453b8513b1ca053f87d93eb4893c7476
david <Davids-MacBook-Pro.local>
2024-04-24 13:48:56 +1000

    Created home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
+import '@fontsource/roboto/300.css';
+import { Container, TextField } from '@mui/material';
+import Typography from '@mui/material/Typography';
 
 function App() {
 
   return (
     <>
-    <h1>Mars Rover App</h1>
+    <Container maxWidth="sm">
+    <Typography sx={{textAlign : "center", marginTop: 4}} variant="h1" gutterBottom>
+      Mars Rover App
+    </Typography>
+    <Typography sx={{textAlign: "center", marginTop: 10}} variant='h5'>
+      How big is your map?
+    </Typography>
+    <TextField sx = {{marginLeft : 20, marginTop: 2}} id="numberOfSquares" label="Enter squares per side" variant="outlined" />
+    </Container>
     </>
   )
 }
